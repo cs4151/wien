a6586aa6257d1f79ff357db944f2ffce515c7216
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-04-03 16:45:22 +0200

    asynchrone Funktion zum laden von JSON Daten

--- main.js
+++ main.js
@@ -22,6 +22,17 @@ let marker = L.marker([stephansdom.lat, stephansdom.lng]).addTo(map);
 marker.bindPopup(stephansdom.title).openPopup();
 
 //Maßstab hinzufügen
-L.control.scale({imperial: false,
-    
+L.control.scale({
+    imperial: false,
 }).addTo(map)
+
+
+//Sehenswürdigkeiten Standorte in Wien
+async function loadSights(url) {
+   //console.log(url);
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    //console.log(jsondata);
+    L.geoJSON(jsondata).addTo(map);
+}
+loadSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
