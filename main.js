76587be44b828e2197ad1e5043113f676f2b4624
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-04-10 18:13:36 +0200

    popo up für sehenswüedigkeiten

--- main.js
+++ main.js
@@ -65,9 +65,12 @@ async function loadSights(url) {
             });
         },
         onEachFeature: function (feature, layer) {
-            console.log(feature.properties);
+            //console.log(feature.properties);
             layer.bindPopup(`
          <img src="${feature.properties.THUMBNAIL}" alt="*">
+         <h4>${feature.properties.NAME}</h4>
+         <address>${feature.properties.ADRESSE}</address>
+         <a href="${feature.properties.WEITERE_INF}" target="wien">Website</a>
                 `);
         }
     }).addTo(overlays.sights);
